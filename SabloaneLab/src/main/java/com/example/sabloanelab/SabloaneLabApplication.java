95242f351ca1ed40904dd948eebef0724b0718f6
DariusHoalba <hoalbadarius@gmail.com>
2023-10-10 08:22:48 +0300

    Setup skeleton

---
+++ SabloaneLab/src/main/java/com/example/sabloanelab/SabloaneLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.sabloanelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabloaneLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SabloaneLabApplication.class, args);
+    }
+
+}
