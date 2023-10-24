a0d005129d178a1227346d4fae0b95bbf9580f7c
DariusHoalba <darius.hoalba02@e-uvt.ro>
2023-10-24 08:59:46 +0300

    Implemented proxy pattern

--- SabloaneLab/src/main/java/com/example/sabloanelab/Image.java
+++ SabloaneLab/src/main/java/com/example/sabloanelab/Image.java
@@ -1,15 +1,35 @@
 package com.example.sabloanelab;
 
-public class Image implements Element{
-    private String imagename;
+import java.util.concurrent.TimeUnit;
 
-    Image(String imagename){
-        this.imagename = imagename;
+public class Image implements Element{
+    private String url;
+    private String name;
+
+    Image(String url, String name){
+        this.url = url;
+        this.name = name;
+        try{
+            TimeUnit.SECONDS.sleep(5);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    Image(String url){
+        this.url = url;
+        try{
+            TimeUnit.SECONDS.sleep(5);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void print(){
-        System.out.println("Image with name: " + imagename);
+        System.out.println("Image with URL: " + url) ;
     }
 
     @Override
---
+++ SabloaneLab/src/main/java/com/example/sabloanelab/Picture.java
@@ -0,0 +1,9 @@
+package com.example.sabloanelab;
+
+import java.awt.*;
+
+public interface Picture {
+    String url();
+    Dimension dim();
+    String content();
+}
